ada223fd12ff84f65987a6dd9fd9d8859b3474c3
Desarrollo <desarrollo@MacBook-Air-de-Administrador-2.local>
2020-06-02 21:55:49 -0300

    Se agrega implementacion para obtener autovalores y autovectores a traves del metodo power iteration

--- src/eigen.cpp
+++ src/eigen.cpp
@@ -10,10 +10,19 @@ pair<double, Vector> power_iteration(const Matrix& X, unsigned num_iter, double 
 {
     Vector b = Vector::Random(X.cols());
     double eigenvalue;
-    /***********************
-     * COMPLETAR CODIGO
-     **********************/
 
+    for (int i = 0; i < num_iter; ++i)
+    {
+        Vector Xb = X * b;
+        b = Xb / Xb.norm();
+    }
+
+    Vector Xb = X * b;
+
+    // Calculamos el autovalor correspondiente al autovector
+    eigenvalue = (double) (b.transpose() * Xb) / (b.squaredNorm());
+
+    // Retornamos el autovalor y el autovector normalizado (No entiendo porque devolver b normalizado)
     return make_pair(eigenvalue, b / b.norm());
 }
 
@@ -23,8 +32,20 @@ pair<Vector, Matrix> get_first_eigenvalues(const Matrix& X, unsigned num, unsign
     Vector eigvalues(num);
     Matrix eigvectors(A.rows(), num);
 
-    /***********************
-     * COMPLETAR CODIGO
-     **********************/
+    for (int i = 0; i < num; ++i)
+    {
+        // Obtengo un autovalor y autovector de X
+        pair<double, Vector> eigen_value_vector = power_iteration(A);
+        double eigen_value = eigen_value_vector.first;
+        Vector eigen_vector = eigen_value_vector.second;
+
+        // Pusheo el nuevo autovalor y autovector
+        eigvalues[i] = eigen_value;
+        eigvectors.col(i) = eigen_vector;
+
+        // Reinterpreto A
+        A = A - (eigen_value * (eigen_vector * eigen_vector.transpose()));
+    }
+
     return make_pair(eigvalues, eigvectors);
-}
+}  
