fa028f77e67f37f04ec81bbba39ee3b64cdbf238
mariano.benzo <marianobenzo@gmail.com>
2020-06-03 03:20:16 -0300

    Agrego pca en el constructor de knn

--- src/pca.cpp
+++ src/pca.cpp
@@ -4,31 +4,31 @@
 
 using namespace std;
 
-PCA::PCA(unsigned int n_components)
+PCA::PCA(unsigned int components)
 {
-	n_main_components = n_components;
+    n_components = components;
 }
 
 void PCA::fit(Matrix X)
 {
-	// Calculo la matriz de covarianza de la matriz de entrenamiento
-	Matrix A = X.rowwise() - X.colwise().mean();
-	Matrix cov_matrix = (A.adjoint() * A) / double(X.rows() - 1);
+    // Calculo la matriz de covarianza de la matriz de entrenamiento
+    Matrix A = X.rowwise() - X.colwise().mean();
+    Matrix cov_matrix = (A.adjoint() * A) / double(X.rows() - 1);
 
-	// A partir de la matriz de covarianza, obtengo los primeros n autovectores y autovalores
-	pair<Vector, Matrix> eigen_values_vectors = get_first_eigenvalues(cov_matrix, n_main_components);
+    // A partir de la matriz de covarianza, obtengo los primeros n autovectores y autovalores
+    pair<Vector, Matrix> eigen_values_vectors = get_first_eigenvalues(cov_matrix, n_components);
 
-	// Obtengo los autovectores de la tupla
-	Matrix eigen_vectors = eigen_values_vectors.second;
+    // Obtengo los autovectores de la tupla
+    Matrix eigen_vectors = eigen_values_vectors.second;
 
-	// Creo una matriz auxiliar para retornar
-	Matrix Ret;
+    // Creo una matriz auxiliar para retornar
+    Matrix Ret;
 
-	// Cambio de base a la matriz de entrenamiento
-	Ret = X * eigen_vectors;
+    // Cambio de base a la matriz de entrenamiento
+    Ret = X * eigen_vectors;
 
-	// Retorno la matriz con el cambio de base
-	X = Ret;
+    // Retorno la matriz con el cambio de base
+    X = Ret;
 }
 
 
