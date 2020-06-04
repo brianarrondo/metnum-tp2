798ac9015d64dacd563ea2c54ebeb63690e357fc
Desarrollo <desarrollo@MacBook-Air-de-Administrador-2.local>
2020-06-04 19:35:46 -0300

    Se agrega implementacion para que se parseen los datos de entrenamiento y a testear, y para transformarlos en matrices

--- src/main.cpp
+++ src/main.cpp
@@ -25,29 +25,12 @@ vector<double> split(string line, string delimiter) {
 	return V;
 }
 
-void read_input_params(int argc, char *argv[]) {
-	if (argc < 8) {
-		std::cout << "Debe ingresar ocho parametros de entrada" << std::endl;
-		exit(1);
-	}
-
-	string method = argv[2];
-	string train_set = argv[4];
-	string test_set = argv[6];
-	string classif = argv[8];
-
-	std::cout << std::endl;
-	std::cout<< "Method: " << method << std::endl;
-	std::cout<< "Train_set: " << train_set << std::endl;
-	std::cout<< "Test_set: " << test_set << std::endl;
-	std::cout<< "Classif: " << classif << std::endl;
-	std::cout << std::endl;
-
+Matrix read_csv_to_matrix(string input_csv) {
 	ifstream file;
 
-	file.open(train_set);
+	file.open(input_csv);
 	if (!file) {
-		cout << "Error al abrir el archivo: " << train_set << endl;
+		cout << "Error al abrir el archivo: " << input_csv << endl;
 		exit(1);
 	}
 
@@ -57,6 +40,7 @@ void read_input_params(int argc, char *argv[]) {
 	unsigned int index = 0;
 
 	getline(file, line);
+	vector<vector<double>> M(42000);
 	while (!file.eof()) {
 		getline(file, line);
 
@@ -73,21 +57,42 @@ void read_input_params(int argc, char *argv[]) {
 
 	file.close();
 
-	// PROBANDO AUTOVECTORES Y AUTOVALORES
-	cout << "MATRIZ DE : " << A.rows() << "x" << A.cols() << endl;
-	cout << endl;
-
-	auto eigen_values_vectors = get_first_eigenvalues(A, 6);
-	Vector eigen_values = eigen_values_vectors.first;
-	Matrix eigen_vectors = eigen_values_vectors.second;
-
-	std::cout << "MATRIZ ORIGINAL: \n" << A << std::endl << endl;
-	std::cout << "AUTOVALORES : \n" << eigen_values << std::endl << endl;
-	std::cout << "AUTOVECTORES : \n" << eigen_vectors << std::endl << endl;
+	return A;
 }
 
-int main(int argc, char** argv){
+void read_input_params(int argc, char *argv[]) {
+	if (argc < 8) {
+		cout << "Debe ingresar ocho parametros de entrada" << endl;
+		exit(1);
+	}
+
+	string method = argv[2];
+	string train_set = argv[4];
+	string test_set = argv[6];
+	string classif = argv[8];
+
+	cout << endl;
+	cout<< "Method: " << method << endl;
+	cout<< "Train_set: " << train_set << endl;
+	cout<< "Test_set: " << test_set << endl;
+	cout<< "Classif: " << classif << endl;
+	cout << endl;
+
+	Matrix train = read_csv_to_matrix(train_set);
+	Matrix test = read_csv_to_matrix(test_set);
+
+	cout << "DIM. MATRIZ ENTRENAMIENTO: " << train.rows() << "x" << train.cols() << endl;
+	cout << "DIM. MATRIZ TEST: " << test.rows() << "x" << test.cols() << endl;
+}
+
+int main(int argc, char** argv) {
+
+	auto t0 = clock();
 	read_input_params(argc, argv);
+	auto t1 = clock();
+
+	double time = (double(t1 - t0)/CLOCKS_PER_SEC);
+	cout << "Execution Time (read input params): " << time << endl;
 
 	return 0;
 }
