a1dfa0f3bb029b5953264a074d74f263de9bebbc
mariano.benzo <marianobenzo@gmail.com>
2020-06-04 20:24:02 -0300

    knn no funciona

--- src/knn.cpp
+++ src/knn.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 //#include <chrono>
 #include <iostream>
+#include <utility>
 #include "knn.h"
 #include "pca.h"
 
@@ -15,11 +16,17 @@ KNNClassifier::KNNClassifier(unsigned int neighbors, bool pca)
 
 void KNNClassifier::fit(Matrix X, Matrix y)
 {
+
+    X_train = X;
+    y_train = y;
+
+/*
     // Hago un cambio de base en las matrices X e Y, quedandome las primeras n componentes
     unsigned int components = 25;
     auto pca = PCA(components);
     pca.fit(X);
     pca.fit(y);
+*/
 }
 
 Vector KNNClassifier::predict(Matrix X)
@@ -29,7 +36,19 @@ Vector KNNClassifier::predict(Matrix X)
 
     for (unsigned k = 0; k < X.rows(); ++k)
     {
+        Vector x = X.row(k);
+
         ret(k) = 0;
+        auto min_distance = (x - X_train.row(0)).norm();
+
+        for (unsigned i = 1; i < X_train.rows(); ++i)
+        {
+            auto distance = (x - X_train.row(i)).norm();
+            if (distance < min_distance) {
+                ret(k) = y_train.row(i)[0];
+                min_distance = distance;
+            }
+        }
     }
 
     return ret;
--- src/knn.h
+++ src/knn.h
@@ -5,12 +5,14 @@
 
 class KNNClassifier {
 public:
-    KNNClassifier(unsigned int n_neighbors, bool with_pca);
+    KNNClassifier(unsigned int neighbors, bool pca);
 
     void fit(Matrix X, Matrix y);
 
     Vector predict(Matrix X);
 private:
-	unsigned int n_neighbors;
-	bool with_pca;
+    unsigned int n_neighbors;
+    bool with_pca;
+    Matrix X_train;
+    Matrix y_train;
 };
