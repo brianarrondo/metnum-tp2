40a7014f0cb5ff60783054fdc5ef613025388aae
Desarrollo <desarrollo@MacBook-Air-de-Administrador-2.local>
2020-06-04 21:26:43 -0300

    Se pushean cosas en el main para realizar pruebas sobre knn

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "pca.h"
 #include "eigen.h"
+#include "knn.h"
 #include <fstream>
 #include <string>
 
@@ -60,7 +61,7 @@ Matrix read_csv_to_matrix(string input_csv) {
 	return A;
 }
 
-void read_input_params(int argc, char *argv[]) {
+pair<Matrix, Matrix> read_input_params(int argc, char *argv[]) {
 	if (argc < 8) {
 		cout << "Debe ingresar ocho parametros de entrada" << endl;
 		exit(1);
@@ -83,16 +84,52 @@ void read_input_params(int argc, char *argv[]) {
 
 	cout << "DIM. MATRIZ ENTRENAMIENTO: " << train.rows() << "x" << train.cols() << endl;
 	cout << "DIM. MATRIZ TEST: " << test.rows() << "x" << test.cols() << endl;
+
+	return make_pair(train, test);
+}
+
+Matrix remove_first_column(Matrix X) {
+	unsigned int last = X.cols() - 1;
+	X.col(0).swap(X.col(last));
+	X.conservativeResize(X.rows(), X.cols() - 1);
+
+	for (unsigned int i = 0; i < X.cols() -1; ++i) {
+		X.col(i).swap(X.col(i+1));
+	}
+
+	return X;
+}
+
+Vector get_first_column(Matrix X) {
+	return X.col(0);
 }
 
 int main(int argc, char** argv) {
 
 	auto t0 = clock();
-	read_input_params(argc, argv);
+	pair<Matrix, Matrix> m = read_input_params(argc, argv);
 	auto t1 = clock();
 
 	double time = (double(t1 - t0)/CLOCKS_PER_SEC);
 	cout << "Execution Time (read input params): " << time << endl;
+
+	Matrix X = m.first;
+	// cout << "X: \n" << X << endl;
+
+	Matrix y_train = get_first_column(X);
+	Matrix X_train = remove_first_column(X);
+
+	// cout << "X_train: \n" << X_train << endl;
+	// cout << "y_train: \n" << y_train << endl;
+
+	auto knn = KNNClassifier(10, false);
+	knn.fit(X_train, y_train);
+
+	Matrix test(28, 28);
+	test << 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,82,152,71,51,51,21,41,51,51,51,51,113,193,152,30,0,0,0,0,0,0,0,0,0,0,0,0,0,122,253,252,253,252,223,243,253,252,253,252,253,252,233,30,0,0,0,0,0,0,0,0,0,0,0,0,0,123,102,41,102,102,102,102,102,102,102,162,254,253,142,0,0,0,0,0,0,0,0,0,0,0,0,0,0,203,102,0,0,0,0,0,0,0,0,183,253,212,20,0,0,0,0,0,0,0,0,0,0,0,0,0,0,203,142,0,0,0,0,0,0,0,11,213,254,91,0,0,0,0,0,0,0,0,0,0,0,0,0,0,41,243,102,0,0,0,0,0,0,0,51,252,172,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,41,223,102,0,0,0,0,0,0,0,214,253,102,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,20,20,0,0,0,0,0,0,0,253,252,102,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,62,254,253,41,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,102,253,171,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,163,254,91,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,203,253,50,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,51,253,254,50,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,51,252,253,50,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,51,253,254,50,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,51,252,213,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,152,253,82,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,233,252,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,21,255,253,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,20,253,212,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0;
+
+	cout << test << endl;
+	cout << "Predict: " << endl << knn.predict(test) << endl;
 
 	return 0;
 }
