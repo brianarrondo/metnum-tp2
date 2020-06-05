db1555ca09cd59680c9035de18be7205d67ae49d
Desarrollo <desarrollo@MacBook-Air-de-Administrador-2.local>
2020-06-05 00:55:01 -0300

    Se distribuye la implementacion de los metodos fit y transform de PCA

--- src/pca.cpp
+++ src/pca.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ #include <iostream>
 #include "pca.h"
 #include "eigen.h"
 
@@ -6,34 +6,25 @@ using namespace std;
 
 PCA::PCA(unsigned int components)
 {
-    n_components = components;
+	n_components = components;
 }
 
 void PCA::fit(Matrix X)
 {
-    // Calculo la matriz de covarianza de la matriz de entrenamiento
-    Matrix A = X.rowwise() - X.colwise().mean();
-    Matrix cov_matrix = (A.adjoint() * A) / double(X.rows() - 1);
+	// Calculo la matriz de covarianza de la matriz de entrenamiento
+	Matrix A = X.rowwise() - X.colwise().mean();
+	cov_matrix = (A.adjoint() * A) / double(X.rows() - 1);
 
-    // A partir de la matriz de covarianza, obtengo los primeros n autovectores y autovalores
-    pair<Vector, Matrix> eigen_values_vectors = get_first_eigenvalues(cov_matrix, n_components);
+	// A partir de la matriz de covarianza, obtengo los primeros n autovectores y autovalores
+	pair<Vector, Matrix> eigen_values_vectors = get_first_eigenvalues(cov_matrix, n_components);
 
-    // Obtengo los autovectores de la tupla
-    Matrix eigen_vectors = eigen_values_vectors.second;
-
-    // Creo una matriz auxiliar para retornar
-    Matrix Ret;
-
-    // Cambio de base a la matriz de entrenamiento
-    Ret = X * eigen_vectors;
-
-    // Retorno la matriz con el cambio de base
-    X = Ret;
+	// Me guardo los autovectores de la tupla
+	eigen_vectors = eigen_values_vectors.second;
 }
 
 
 MatrixXd PCA::transform(Matrix X)
 {
-
-  throw std::runtime_error("Sin implementar");
+	// Retorno la matriz con el cambio de base
+	return X * eigen_vectors;
 }
--- src/pca.h
+++ src/pca.h
@@ -10,4 +10,6 @@ public:
     Eigen::MatrixXd transform(Matrix X);
 private:
     unsigned int n_components;
+    Matrix cov_matrix;
+    Matrix eigen_vectors;
 };
